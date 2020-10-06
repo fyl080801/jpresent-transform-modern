10141bdd0643731351ca679cf1b0ee6ea92f82c9
fengyuanliang3 <fengyuanliang2@jd.com>
2020-10-06 19:36:10 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,1 @@
+# jpresent-transform-modern
---
+++ build/webpack.config.prod.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const merge = require('webpack-merge')
+const nodeExternals = require('webpack-node-externals')
+const common = require('./webpack.config.common')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
+const production = {
+  entry: path.resolve(__dirname, '../lib/index.js'),
+  devtool: 'source-map',
+  mode: 'production',
+  optimization: {
+    minimize: true,
+  },
+}
+
+const plugins = []
+
+if (process.env.npm_config_report) {
+  plugins.push(new BundleAnalyzerPlugin())
+}
+
+module.exports = [
+  merge(common, production, {
+    plugins: plugins,
+    output: {
+      path: path.resolve(__dirname, '../dist'),
+      filename: 'index.umd.js',
+      libraryTarget: 'umd',
+      library: 'jpresent-transform-modern',
+      umdNamedDefine: true,
+    },
+  }),
+  merge(common, production, {
+    externals: [nodeExternals()],
+    output: {
+      path: path.resolve(__dirname, '../dist'),
+      filename: 'index.common.js',
+      libraryTarget: 'commonjs2',
+      library: 'jpresent-transform-modern',
+    },
+  }),
+]
